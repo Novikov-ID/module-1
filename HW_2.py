c391a1990975156fb295e326bfe8724bed084414
Novikov-ID <novikov.id@gmail.com>
2024-08-14 19:46:06 +0300

    Add files via upload

--- HW_2.py
+++ HW_2.py
@@ -1,6 +1,5 @@
-example = 'противоречие'
-print(example[0])
-print(example[-1])
-print(example[5:])
-print(example[::-1])
-print(example[1::2])
+quantity_homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / quantity_homework
+print('Курс: ' + course_name + ',', 'всего задач:' + str(quantity_homework) + ',' , 'затрачено часов: ' + str(hours_spent) + ',', 'среднее время выполнения', time_per_task, 'часа.')
