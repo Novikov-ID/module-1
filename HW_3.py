29e7ca65e819291ba6709465d5a14121814ff3fa
Novikov-ID <novikov.id@gmail.com>
2024-08-14 19:52:30 +0300

    Delete HW_3.py

--- HW_3.py
+++
@@ -1,5 +0,0 @@
-quantity_homework = 12
-hours_spent = 1.5
-course_name = 'Python'
-time_per_task = hours_spent / quantity_homework
-print('Курс: ' + course_name + ',', 'всего задач:' + str(quantity_homework) + ',' , 'затрачено часов: ' + str(hours_spent) + ',', 'среднее время выполнения', time_per_task, 'часа.')
